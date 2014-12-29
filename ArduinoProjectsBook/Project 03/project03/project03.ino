6cc7ea8aa487adce682493a2260720e706d5e433
acangialosi <acangialosi@hotmail.com>
2014-12-28 22:34:25 -0800

    Complete project

--- ArduinoProjectsBook/Project 03/project03/project03.ino
+++ ArduinoProjectsBook/Project 03/project03/project03.ino
@@ -1,5 +1,5 @@
 const int sensorPin=A0;
-const float baselineTemp = 20.0;
+float baselineTemp = 20.0;
 
 void setup()
 {
@@ -10,10 +10,24 @@ void setup()
     pinMode(pinNumber, OUTPUT);
     digitalWrite(pinNumber, LOW);
   }
+
+  int sensorVal = 0;
+  int sample; 
+  for(sample = 0; sample < 5; sample++)
+  {
+    delay(1);
+    sensorVal += analogRead(sensorPin);
+  }
+  sensorVal = sensorVal / sample;
+  
+  baselineTemp = (((sensorVal/1024.0) * 5.0) - .5) * 100;
+  Serial.print("Baseline Temp: "); Serial.println(baselineTemp);
 }
 
 void loop()
 {
+  Serial.print("Baseline Temp: "); Serial.print(baselineTemp);
+
   int sensorVal = analogRead(sensorPin);
   
   Serial.print("Sensor Value: "); Serial.print(sensorVal);
@@ -25,8 +39,34 @@ void loop()
   Serial.print(", Celciius: "); Serial.print(tempC); 
   
   float tempF = (tempC * 9/5) + 32;
-  Serial.print(", fFahrenheit: "); Serial.print(tempF); 
+  Serial.print(", Fahrenheit: "); Serial.print(tempF); 
   
-  Serial.print("\r\n");
-  delay(250);
+  Serial.println("");
+  
+  if(tempC < baselineTemp)
+  {
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+  }
+  else if(tempC >= baselineTemp + 2 && tempC < baselineTemp + 4)
+  {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+  }  
+  else if(tempC >= baselineTemp + 4 && tempC < baselineTemp + 6)
+  {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW);
+  }
+  else if(tempC >= baselineTemp + 6)
+  {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+  }    
+  
+  delay(1);
 }
