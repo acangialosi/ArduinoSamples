02aae1939b60c726aa5df1339233faaaf6379971
Anthony Cangialosi <acangialosi@users.noreply.github.com>
2015-01-15 01:06:35 -0800

    Add code to test ultrasonic sensor

--- Ultra Sensor/testultra.ino
+++ Ultra Sensor/testultra.ino
@@ -1,13 +1,57 @@
-#include <Ultra.h>
+class Ultra
+{
+  int trig,echo;
+
+public:
+  Ultra(int pin1, int pin2)
+  {
+    pinMode(pin1,OUTPUT);
+    pinMode(pin2,INPUT);
+    trig = pin1;
+    echo = pin2;
+
+  }
+
+  long Time()
+  {
+    digitalWrite(trig,HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(trig,LOW);
+    return(pulseIn(echo,HIGH));
+  }
+  long Range()
+  {
+    long x,y;
+    digitalWrite(trig,HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(trig,LOW);
+    x = pulseIn(echo,HIGH);
+    y= (x/2)/29.1;
+    return(y);
+
+  }
+
+};
+
+Ultra AKR(2,3);
+String strTime, strRange, strOutput;
 
 void setup()
 {
-	Serial.begin(9600);
+  Serial.begin(9600);
+  strTime = "Time; ";
+  strRange = "Range: ";
+  strOutput = String();
 }
 
 void loop()
-{
+{  
+  strOutput = strTime + AKR.Time();
+  Serial.println(strOutput);
+  
+  strOutput = strRange + AKR.Range() + "CM";
+  Serial.println(strOutput);
 
-  /* add main program code here */
-
+  delay(1000);
 }
+
